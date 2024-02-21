d13f5aa63be578b830cce5b5d25f3b0e5497a452
Aditya Sharma <aditya.sharma@kellogg.northwestern.edu>
2024-02-21 11:43:04 +0000

    Baseline starter app activated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Place.create(name: "Place 1")
+Place.create(name: "Place 2")
